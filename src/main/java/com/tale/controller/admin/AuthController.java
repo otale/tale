cb2d20be7aa5e3fcea347d2daec5b617f890ebbe
biezhi <biezhi.me@gmail.com>
2017-02-26 01:14:15 +0800

    🦄 fix https://github.com/otale/tale/issues/10 close https://github.com/otale/tale/issues/10

--- src/main/java/com/tale/controller/admin/AuthController.java
+++ src/main/java/com/tale/controller/admin/AuthController.java
@@ -11,6 +11,7 @@ import com.blade.mvc.http.Request;
 import com.blade.mvc.http.Response;
 import com.blade.mvc.http.wrapper.Session;
 import com.blade.mvc.view.RestResponse;
+import com.tale.controller.BaseController;
 import com.tale.dto.LogActions;
 import com.tale.exception.TipException;
 import com.tale.init.TaleConst;
@@ -25,7 +26,7 @@ import org.slf4j.LoggerFactory;
  * Created by biezhi on 2017/2/21.
  */
 @Controller("admin")
-public class AuthController {
+public class AuthController extends BaseController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
 
@@ -47,7 +48,12 @@ public class AuthController {
                                 @QueryParam String remeber_me,
                                 Request request,
                                 Session session, Response response) {
+
+        Integer error_count = cache.get("login_error_count");
         try {
+            if(null != error_count && error_count > 3){
+                return RestResponse.fail("您输入密码已经错误超过3次，请10分钟后尝试");
+            }
             Users user = usersService.login(username, password);
             session.attribute(TaleConst.LOGIN_SESSION_KEY, user);
             if (StringKit.isNotBlank(remeber_me)) {
@@ -55,6 +61,9 @@ public class AuthController {
             }
             logService.save(LogActions.LOGIN, null, request.address(), user.getUid());
         } catch (Exception e) {
+            error_count = null == error_count ? 1 : error_count + 1;
+            cache.set("login_error_count", error_count, 10 * 60);
+
             String msg = "登录失败";
             if (e instanceof TipException) {
                 msg = e.getMessage();
