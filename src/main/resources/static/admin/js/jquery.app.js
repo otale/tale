f23564cab5fe9a6261dc747ea57af364c1c2a3a5
biezhi <biezhi.me@gmail.com>
2017-02-26 13:13:04 +0800

    🦄 fix https://github.com/otale/tale/issues/16 and merge admin css

--- src/main/resources/static/admin/js/jquery.app.js
+++ src/main/resources/static/admin/js/jquery.app.js
@@ -268,15 +268,3 @@ function toggle_slimscroll(item) {
         $(item).siblings(".slimScrollBar").css("visibility", "visible");
     }
 }
-/* === following js will activate the menu in left side bar based on url ==== */
-$(document).ready(function () {
-    // $("#sidebar-menu a").each(function () {
-    //     if (this.href == window.location.href) {
-    //         $(this).addClass("active");
-    //         console.log($(this).text());
-    //         $(this).parent().addClass("active"); // add active to li of the current link
-    //         $(this).parent().parent().prev().addClass("active"); // add active class to an anchor
-    //         $(this).parent().parent().prev().click(); // click the item to make it drop
-    //     }
-    // });
-});
