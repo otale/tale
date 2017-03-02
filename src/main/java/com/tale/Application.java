5e8215d3644bce0b153ea04ab87e4107df6bed6d
biezhi <biezhi.me@gmail.com>
2017-03-02 16:51:16 +0800

    🦄 fix https://github.com/otale/tale/issues/77

--- src/main/java/com/tale/Application.java
+++ src/main/java/com/tale/Application.java
@@ -1,9 +1,6 @@
 package com.tale;
 
 import com.tale.init.TaleLoader;
-import com.tale.utils.ExtClasspathLoader;
-
-import java.io.File;
 
 import static com.blade.Blade.$;
 
@@ -11,7 +8,6 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
         TaleLoader.init();
-        TaleLoader.loadPlugin(new File("/Users/biezhi/workspace/projects/java/plugin_upyun/target/plguin_upyun.jar"));
         $().start(Application.class);
     }
 
