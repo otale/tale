972306087bc403ae9e965c119d2509bc48bf61da
biezhi <biezhi.me@gmail.com>
2017-03-05 23:12:15 +0800

    🦄 remove local upyun plugin

--- src/main/java/com/tale/Application.java
+++ src/main/java/com/tale/Application.java
@@ -1,9 +1,6 @@
 package com.tale;
 
-import com.blade.Blade;
 import com.tale.init.TaleLoader;
-
-import java.io.File;
 
 import static com.blade.Blade.$;
 
@@ -11,7 +8,6 @@ public class Application {
 
     public static void main(String[] args) throws Exception {
         TaleLoader.init();
-        TaleLoader.loadPlugin(Blade.$().bConfig(), new File("/Users/biezhi/workspace/projects/java/plugin_upyun/target/plguin_upyun.jar"));
         $().start(Application.class);
     }
 
