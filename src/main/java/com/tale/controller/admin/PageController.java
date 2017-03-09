e63c19a31bd64e33a0e7718face4fa1d7d882ace
biezhi <biezhi.me@gmail.com>
2017-03-09 22:46:56 +0800

    🦄 fix page layout

--- src/main/java/com/tale/controller/admin/PageController.java
+++ src/main/java/com/tale/controller/admin/PageController.java
@@ -11,6 +11,7 @@ import com.tale.controller.BaseController;
 import com.tale.dto.LogActions;
 import com.tale.dto.Types;
 import com.tale.exception.TipException;
+import com.tale.ext.Commons;
 import com.tale.init.TaleConst;
 import com.tale.model.Contents;
 import com.tale.model.Users;
@@ -50,6 +51,7 @@ public class PageController extends BaseController {
 
     @Route(value = "new", method = HttpMethod.GET)
     public String newPage(Request request) {
+        request.attribute(Types.ATTACH_URL, Commons.site_option(Types.ATTACH_URL, Commons.site_url()));
         return "admin/page_edit";
     }
 
@@ -57,6 +59,7 @@ public class PageController extends BaseController {
     public String editPage(@PathParam String cid, Request request) {
         Contents contents = contentsService.getContents(cid);
         request.attribute("contents", contents);
+        request.attribute(Types.ATTACH_URL, Commons.site_option(Types.ATTACH_URL, Commons.site_url()));
         return "admin/page_edit";
     }
 
@@ -64,7 +67,8 @@ public class PageController extends BaseController {
     @JSON
     public RestResponse publishPage(@QueryParam String title, @QueryParam String content,
                                     @QueryParam String status, @QueryParam String slug,
-                                    @QueryParam Integer allow_comment, @QueryParam Integer allow_ping) {
+                                    @QueryParam String fmt_type,
+                                    @QueryParam Boolean allow_comment) {
 
         Users users = this.user();
         Contents contents = new Contents();
@@ -72,13 +76,10 @@ public class PageController extends BaseController {
         contents.setContent(content);
         contents.setStatus(status);
         contents.setSlug(slug);
+        contents.setFmt_type(fmt_type);
         contents.setType(Types.PAGE);
-        if (null != allow_comment) {
-            contents.setAllow_comment(allow_comment == 1);
-        }
-        if (null != allow_ping) {
-            contents.setAllow_ping(allow_ping == 1);
-        }
+        contents.setAllow_comment(allow_comment);
+        contents.setAllow_ping(true);
         contents.setAuthor_id(users.getUid());
 
         try {
@@ -99,9 +100,9 @@ public class PageController extends BaseController {
     @Route(value = "modify", method = HttpMethod.POST)
     @JSON
     public RestResponse modifyArticle(@QueryParam Integer cid, @QueryParam String title,
-                                      @QueryParam String content,
+                                      @QueryParam String content,@QueryParam String fmt_type,
                                       @QueryParam String status, @QueryParam String slug,
-                                      @QueryParam Integer allow_comment, @QueryParam Integer allow_ping) {
+                                      @QueryParam Boolean allow_comment) {
 
         Users users = this.user();
         Contents contents = new Contents();
@@ -109,14 +110,11 @@ public class PageController extends BaseController {
         contents.setTitle(title);
         contents.setContent(content);
         contents.setStatus(status);
+        contents.setFmt_type(fmt_type);
         contents.setSlug(slug);
         contents.setType(Types.PAGE);
-        if (null != allow_comment) {
-            contents.setAllow_comment(allow_comment == 1);
-        }
-        if (null != allow_ping) {
-            contents.setAllow_ping(allow_ping == 1);
-        }
+        contents.setAllow_comment(allow_comment);
+        contents.setAllow_ping(true);
         contents.setAuthor_id(users.getUid());
         try {
             contentsService.updateArticle(contents);
