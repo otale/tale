ffd98d28adb0d1e4dd63c3cfad359984de146a17
xiwenAndlejian <94333236@qq.com>
2018-01-14 23:07:47 +0800

    :up:发包脚本更新

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+## 替换为非开发模式 TODO 正则替换
+sed -i '' "s/app.devMode=true/app.devMode=false/g" src/main/resources/app.properties
 
 ## 发包脚本
 mvn clean package -Pprod -Dmaven.test.skip=true
+
+## 替换为开发模式 TODO 正则替换
+sed -i '' "s/app.devMode=false/app.devMode=true/g" src/main/resources/app.properties
+
+## 发包
+scp -rp target/dist root@aliyun:/home/ganx/wars
