826525b8eb5102b90571b486e06bc151c0d90442
王爵 nice <biezhi.me@gmail.com>
2018-10-07 14:37:55 +0800

    :bento: add tool scripts

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+APP_NAME="tale"
+get_latest_release() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
+
+TAG_VERSION=$(get_latest_release "otale/tale")
+
+wget -N --no-check-certificate https://github.com/otale/tale/releases/download/$TAG_VERSION/tale.tar.gz
+
+echo '下载完毕'
+
+mkdir $APP_NAME
+tar -zxvf $APP_NAME.tar.gz -C $APP_NAME && cd $APP_NAME
+chmod +x tool
+sh tool start
