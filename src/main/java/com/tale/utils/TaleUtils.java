eaa62397a73e6d041273047ee81c284a0188c274
王爵 nice <biezhi.me@gmail.com>
2018-10-07 16:05:07 +0800

    :art: update page path length

--- src/main/java/com/tale/utils/TaleUtils.java
+++ src/main/java/com/tale/utils/TaleUtils.java
@@ -58,7 +58,7 @@ public class TaleUtils {
     private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
             Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
 
-    private static final Pattern SLUG_REGEX = Pattern.compile("^[A-Za-z0-9_-]{5,100}$", Pattern.CASE_INSENSITIVE);
+    private static final Pattern SLUG_REGEX = Pattern.compile("^[A-Za-z0-9_-]{3,50}$", Pattern.CASE_INSENSITIVE);
 
     /**
      * 设置记住密码cookie
