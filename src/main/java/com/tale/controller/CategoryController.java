66994a5bb8842ab2be1a46629a23ffa3b7a3964e
zaoangod <zaoangod@gmail.com>
2018-11-04 15:13:27 +0800

    Update CategoryController.java

--- src/main/java/com/tale/controller/CategoryController.java
+++ src/main/java/com/tale/controller/CategoryController.java
@@ -35,6 +35,92 @@ public class CategoryController extends BaseController {
 
     /**
      * 分类列表页
+     */
+    @GetRoute(value = {"category", "category.html"})
+    public String category(Request request) {
+        Map<String, List<Contents>> mapping = metasService.getMetaMapping(Types.CATEGORY);
+        Set<String> category = mapping.keySet();
+        request.attribute("category", category);
+        request.attribute("mapping", mapping);
+        request.attribute("is_category", true);
+        return this.render("category");
+    }
+
+    /**
+     * 某个分类详情页
+     */
+    @GetRoute(value = {"category/:keyword", "category/:keyword.html"})
+    public String category(Request request, @PathParam String keyword, @Param(defaultValue = "12") int limit) {
+        return this.categoryPage(request, keyword, 1, limit);
+    }
+
+    /**
+     * 某个分类详情页分页
+     */
+    @GetRoute(value = {"category/:keyword/:page", "category/:keyword/:page.html"})
+    public String categoryPage(Request request, @PathParam String keyword, @PathParam int page, @Param(defaultValue = "12") int limit) {
+        page = page < 0 || page > TaleConst.MAX_PAGE ? 1 : page;
+        Metas metaDto = metasService.getMeta(Types.CATEGORY, keyword);
+        if (null == metaDto) {
+            return this.render_404();
+        }
+        Page<Contents> contentsPage = contentsService.getArticles(metaDto.getMid(), page, limit);
+        request.attribute("articles", contentsPage);
+        request.attribute("meta", metaDto);
+        request.attribute("type", "分类");
+        request.attribute("keyword", keyword);
+        request.attribute("is_category", true);
+        request.attribute("page_prefix", "/category/" + keyword);
+        return this.render("category");
+    }
+
+    /**
+     * 标签列表页面
+     * <p>
+     * 渲染所有的标签和文章映射
+     */
+    @GetRoute(value = {"tag", "tag.html"})
+    public String tag(Request request) {
+        Map<String, List<Contents>> mapping = metasService.getMetaMapping(Types.TAG);
+        Set<String> tags = mapping.keySet();
+        request.attribute("tag", tags);
+        request.attribute("mapping", mapping);
+        request.attribute("is_tag", true);
+        return this.render("tag");
+    }
+
+    /**
+     * 标签详情页
+     *
+     * @param name 标签名
+     */
+    @GetRoute(value = {"tag/:name", "tag/:name.html"})
+    public String tag(Request request, @PathParam String name, @Param(defaultValue = "12") int limit) {
+        return this.tagPage(request, name, 1, limit);
+    }
+
+    /**
+     * 标签下文章分页
+     */
+    @GetRoute(value = {"tag/:name/:page", "tag/:name/:page.html"})
+    public String tagPage(Request request, @PathParam String name, @PathParam int page, @Param(defaultValue = "12") int limit) {
+        page = page < 0 || page > TaleConst.MAX_PAGE ? 1 : page;
+        Metas metaDto = metasService.getMeta(Types.TAG, name);
+        if (null == metaDto) {
+            return this.render_404();
+        }
+        Page<Contents> contentsPage = contentsService.getArticles(metaDto.getMid(), page, limit);
+        request.attribute("articles", contentsPage);
+        request.attribute("meta", metaDto);
+        request.attribute("type", "标签");
+        request.attribute("keyword", name);
+        request.attribute("is_tag", true);
+        request.attribute("page_prefix", "/tag/" + name);
+        return this.render("tag");
+    }
+
+    /**
+     * 分类列表页
      *
      * @since 1.3.1
      */
