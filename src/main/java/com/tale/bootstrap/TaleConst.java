8a9d698f4e0a2f6b771a4db6b9aaee1a120c9e73
Yan Wenjie <yanwenjie@zidanduanxin.com>
2018-11-21 19:09:35 +0800

    添加评论邮件他通知功能

--- src/main/java/com/tale/bootstrap/TaleConst.java
+++ src/main/java/com/tale/bootstrap/TaleConst.java
@@ -1,6 +1,5 @@
 package com.tale.bootstrap;
 
-
 import com.blade.Environment;
 import com.tale.controller.admin.AdminApiController;
 import com.tale.model.dto.PluginMenu;
@@ -15,16 +14,17 @@ import java.util.*;
  */
 public class TaleConst {
 
-    public static final String CLASSPATH = new File(AdminApiController.class.getResource("/").getPath()).getPath() + File.separatorChar;
+    public static final String CLASSPATH =
+        new File(AdminApiController.class.getResource("/").getPath()).getPath() + File.separatorChar;
 
-    public static final String      REMEMBER_IN_COOKIE = "remember_me";
-    public static final String      LOGIN_ERROR_COUNT  = "login_error_count";
-    public static       String      LOGIN_SESSION_KEY  = "login_user";
-    public static       String      REMEMBER_TOKEN     = "";
-    public static       Environment OPTIONS            = Environment.of(new HashMap<>());
-    public static       Boolean     INSTALLED          = false;
-    public static       Boolean     ENABLED_CDN        = true;
-    public static       Environment BCONF              = null;
+    public static final String REMEMBER_IN_COOKIE = "remember_me";
+    public static final String LOGIN_ERROR_COUNT = "login_error_count";
+    public static String LOGIN_SESSION_KEY = "login_user";
+    public static String REMEMBER_TOKEN = "";
+    public static Environment OPTIONS = Environment.of(new HashMap<>());
+    public static Boolean INSTALLED = false;
+    public static Boolean ENABLED_CDN = true;
+    public static Environment BCONF = null;
 
     /**
      * 最大页码
@@ -61,10 +61,10 @@ public class TaleConst {
      */
     public static final Set<String> BLOCK_IPS = new HashSet<>(16);
 
-    public static final String SLUG_HOME        = "/";
-    public static final String SLUG_ARCHIVES    = "archives";
+    public static final String SLUG_HOME = "/";
+    public static final String SLUG_ARCHIVES = "archives";
     public static final String SLUG_CATEGRORIES = "categories";
-    public static final String SLUG_TAGS        = "tags";
+    public static final String SLUG_TAGS = "tags";
 
     /**
      * 静态资源URI
@@ -92,29 +92,40 @@ public class TaleConst {
     public static final String PLUGINS_MENU_NAME = "plugin_menus";
 
     public static final String ENV_SUPPORT_163_MUSIC = "app.support_163_music";
-    public static final String ENV_SUPPORT_GIST      = "app.support_gist";
-    public static final String MP3_PREFIX            = "[mp3:";
-    public static final String MUSIC_IFRAME          = "<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=350 height=106 src=\"//music.163.com/outchain/player?type=2&id=$1&auto=0&height=88\"></iframe>";
-    public static final String MUSIC_REG_PATTERN     = "\\[mp3:(\\d+)\\]";
-    public static final String GIST_PREFIX_URL       = "https://gist.github.com/";
-    public static final String GIST_REG_PATTERN      = "&lt;script src=\"https://gist.github.com/(\\w+)/(\\w+)\\.js\">&lt;/script>";
-    public static final String GIST_REPLATE_PATTERN  = "<script src=\"https://gist.github.com/$1/$2\\.js\"></script>";
+    public static final String ENV_SUPPORT_GIST = "app.support_gist";
+    public static final String MP3_PREFIX = "[mp3:";
+    public static final String MUSIC_IFRAME =
+        "<iframe frameborder=\"no\" border=\"0\" marginwidth=\"0\" marginheight=\"0\" width=350 height=106 src=\"//music.163.com/outchain/player?type=2&id=$1&auto=0&height=88\"></iframe>";
+    public static final String MUSIC_REG_PATTERN = "\\[mp3:(\\d+)\\]";
+    public static final String GIST_PREFIX_URL = "https://gist.github.com/";
+    public static final String GIST_REG_PATTERN =
+        "&lt;script src=\"https://gist.github.com/(\\w+)/(\\w+)\\.js\">&lt;/script>";
+    public static final String GIST_REPLATE_PATTERN = "<script src=\"https://gist.github.com/$1/$2\\.js\"></script>";
 
+    public static final String SQL_QUERY_METAS =
+        "select a.*, count(b.cid) as count from t_metas a left join `t_relationships` b on a.mid = b.mid "
+            + "where a.type = ? and a.name = ? group by a.mid";
 
-    public static final String SQL_QUERY_METAS = "select a.*, count(b.cid) as count from t_metas a left join `t_relationships` b on a.mid = b.mid " +
-            "where a.type = ? and a.name = ? group by a.mid";
-
-    public static final String SQL_QUERY_ARTICLES = "select a.* from t_contents a left join t_relationships b on a.cid = b.cid " +
-            "where b.mid = ? and a.status = 'publish' and a.type = 'post' order by a.created desc";
+    public static final String SQL_QUERY_ARTICLES =
+        "select a.* from t_contents a left join t_relationships b on a.cid = b.cid "
+            + "where b.mid = ? and a.status = 'publish' and a.type = 'post' order by a.created desc";
 
     public static final String COMMENT_APPROVED = "approved";
     public static final String COMMENT_NO_AUDIT = "no_audit";
 
-    public static final String OPTION_CDN_URL             = "cdn_url";
-    public static final String OPTION_SITE_THEME          = "site_theme";
-    public static final String OPTION_ALLOW_INSTALL       = "allow_install";
+    public static final String OPTION_CDN_URL = "cdn_url";
+    public static final String OPTION_SITE_THEME = "site_theme";
+    public static final String OPTION_ALLOW_INSTALL = "allow_install";
     public static final String OPTION_ALLOW_COMMENT_AUDIT = "allow_comment_audit";
-    public static final String OPTION_ALLOW_CLOUD_CDN     = "allow_cloud_CDN";
-    public static final String OPTION_SAFE_REMEMBER_ME    = "safe_remember_me";
+    public static final String OPTION_ALLOW_CLOUD_CDN = "allow_cloud_CDN";
+    public static final String OPTION_SAFE_REMEMBER_ME = "safe_remember_me";
 
+    /**
+     * 邮件发送相关常量
+     */
+    //收件人邮箱地址，这里建议和发件邮箱地址相同，即将邮件转发给自己，这样就不会被邮件服务器识别为垃圾邮件而退信了
+    public static final String TO_MAIL_ADDRESS = "";
+    public static final String FROM_ADDRESS = ""; //发件人邮箱地址
+    public static final String USER = ""; //发件人称号，同邮箱地址
+    public static final String PSW = ""; //发件人邮箱客户端授权码
 }
---
+++ src/main/java/com/tale/service/MailService.java
@@ -0,0 +1,56 @@
+package com.tale.service;
+
+import com.blade.ioc.annotation.Bean;
+import com.tale.bootstrap.TaleConst;
+
+import javax.mail.BodyPart;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Properties;
+
+/**
+ * @author CarryJey
+ * @since 2018/11/18
+ */
+@Bean
+public class MailService {
+    /**
+     * 163邮箱服务器发送邮件
+     */
+    public boolean sendMail(String to, String text, String title) {
+        Properties props = new Properties();
+        props.setProperty("mail.smtp.host", "smtp.163.com"); // 设置发送邮件的邮件服务器的属性（这里使用网易的smtp服务器）
+        props.put("mail.smtp.host", "smtp.163.com"); // 需要经过授权，也就是有户名和密码的校验，这样才能通过验证（一定要有这一条）
+        props.put("mail.smtp.auth", "true"); // 用刚刚设置好的props对象构建一个session
+        Session session = Session.getDefaultInstance(props); // 有了这句便可以在发送邮件的过程中在console处显示过程信息，供调试使
+        // 用（你可以在控制台（console)上看到发送邮件的过程）
+        session.setDebug(true); // 用session为参数定义消息对象
+        MimeMessage message = new MimeMessage(session); // 加载发件人地址
+        try {
+            message.setFrom(new InternetAddress(TaleConst.FROM_ADDRESS));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to)); // 加载收件人地址
+            message.setSubject(title); // 加载标题
+            Multipart multipart = new MimeMultipart(); // 向multipart对象中添加邮件的各个部分内容，包括文本内容和附件
+            BodyPart contentPart = new MimeBodyPart(); // 设置邮件的文本内容
+            contentPart.setContent(text, "text/html;charset=utf-8");
+            multipart.addBodyPart(contentPart);
+            message.setContent(multipart);
+            message.saveChanges(); // 保存变化
+            Transport transport = session.getTransport("smtp"); // 连接服务器的邮箱
+            transport.connect("smtp.163.com", TaleConst.USER, TaleConst.PSW); // 把邮件发送出去
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
